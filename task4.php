c42fbdda5221285e54e7c99388c62e5b67504508
T De Haro <Tochitos2@gmail.com>
2020-04-20 17:04:08 +0100

    Fixed HTML table generation.

--- task4.php
+++ task4.php
@@ -1,1 +1,26 @@
 <?php
+try {
+    $dbhandle = new PDO('mysql:host=dragon.kent.ac.uk; dbname=co323',
+        'co323', 'h@v3fun');
+}
+catch (PDOException $e) { die('DB connect error: ' . $e->getMessage()); }
+
+$sql = "SELECT   name, weighting
+FROM     Assessment a JOIN Course c ON a.cid = c.cid 
+WHERE    title = 'Database Systems'
+ORDER BY name;"; // The SQL query itself
+$query = $dbhandle->prepare($sql); // Prepare and ...
+if ( $query->execute() === FALSE ) { // ... execute the query
+    die('Query exec error: ' . implode($query->errorInfo(),' '));
+}
+$results = $query->fetchAll(); // Put all the results in an array
+?>
+    <h2>Database Systems Assessments</h2>
+    <table><tr><th>Name</th><th>Weighting</th></tr>
+        <?php // Generate HTML from the contents of the results array
+        foreach ($results as $row) {
+            echo "<tr><td>".$row['name']."</td>"."<td>".$row['weighting']."</td></tr>";
+        }
+        ?>
+    </table>
+
